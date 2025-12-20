f326e06168da9dd8c5af4204ec8822644e618c3a
BlueBerryBug <24je0220@iitism.ac.in>
2025-12-21 00:06:37 +0530

    configruing the target end point in frontend

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://ranksetgoa-app-backend.onrender.com",
         changeOrigin: true,
         secure: false,
       },
